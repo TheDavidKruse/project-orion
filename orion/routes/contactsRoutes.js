21e6d7868e98714ce58271a97daef29b3d068475
Shu Sia Lukito <lukitos@yahoo.com>
2017-07-17 16:19:49 -0700

    Initializing contacts route

---
+++ orion/routes/contactsRoutes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex');
+
+/*
+  GET all contacts
+*/
+router.get('/', function (req, res, next) {
+  res.send('Showing all contacts');
+});
+
+module.exports = router;
