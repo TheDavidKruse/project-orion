a76999472045c76bccf7ef1fc36a86888e4b4f15
Drienn <Emitdutcher@gmail.com>
2017-07-18 13:36:31 -0700

    added staff routes

--- orion/routes/staffRoutes.js
+++ orion/routes/staffRoutes.js
@@ -2,11 +2,38 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-/*
-  GET all contacts
-*/
+// GET All Staff
 router.get('/', function(req, res, next) {
-  res.send('Showing all contacts');
+  knex('staff').select().then(staff => res.render('staff', {
+    staff,
+  }));
 });
+
+
+//GET staff by ID
+router.get('/:id', function(req, res, next) {
+  knex('staff').select().where('id', req.params.id).then(staff => res.render('staff', {
+    staffs
+  }));
+});
+
+//GET All Students related to staff
+router.get('/:id/students', function(req, res, next) {
+  knex.raw(`select concat (staff.first_name,' ', staff.last_name) as staff_name, concat (student.first_name,' ', student.last_name) as student_name, student.email as student_email
+from student, staff
+where staff.id = student.staff_id and student.staff_id = ${req.params.id}`).then(staff => res.render('staff', {
+    staff: staff.rows
+  }))
+})
+
+//POST UPDATE staff by ID
+router.post('/:id', function(req, res, next) {
+  knex('staff').update(req.body).where('id', req.params.id).then(staff => res.render('staff', {
+    staff
+  }));
+});
+
+
+
 
 module.exports = router;
