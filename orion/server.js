7b4090760ed680efc29d8c186eb9d91740c91e77
theDavidKruse <thedavidkruse@gmail.com>
2017-07-18 10:15:51 -0700

    Created partials and finished(?) login

--- orion/server.js
+++ orion/server.js
@@ -5,6 +5,7 @@ var port = process.env.PORT || 8000;
 var cors = require('cors');
 var logger = require('morgan');
 var knex = require('./db/knex');
+var expressEJSLayouts = require('express-ejs-layouts');
 
 var index = require('./routes/indexRoutes');
 var todos = require('./routes/todosRoutes');
@@ -14,10 +15,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
 app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressEJSLayouts);
 
 app.use('/', index);
 app.use('/todos', todos);
