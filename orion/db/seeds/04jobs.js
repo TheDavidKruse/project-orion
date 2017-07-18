f70f61f8bbb49d6e037f8d890d50f91eca6a8a96
Shu Sia Lukito <lukitos@yahoo.com>
2017-07-18 11:52:37 -0700

    Added company/jobs routes

--- orion/db/seeds/04jobs.js
+++ orion/db/seeds/04jobs.js
@@ -22,66 +22,77 @@ exports.seed = function(knex, Promise) {
           "student_id": 1
         },
         {
-            "company": "Duder Mifflin Paper Company",
-            "job_link": "Paper.com",
-            "industry": "Making paper",
-            "location": "W. OFFCIE SPACE Rd. 999",
-            "job_title": "Printing paper",
-            "hiring_manager": "Milton",
-            "direct_contact_manager": false,
-            "recruting_lead": "Lunberg",
-            "direct_recruiting_manager": true,
-            "date_applied": "Mon Jul 17 2017",
-            "date_phone_interview": "Mon Jul 17 2017",
-            "date_submit": "Mon Jul 17 2017",
-            "date_interview": "Wed Jul 19 2017",
-            "date_followup": "Wed Jul 19 2017",
-            "comments": "if you could stay late thatd be great",
-            "student_id": 2
-          },
-          {
-              "company": "Pied piper",
-              "job_link": "piedpiper.com",
-              "industry": "Data compression",
-              "location": "Silicon Valley",
-              "job_title": "CTO",
-              "hiring_manager": "Erlech Bachman",
-              "direct_contact_manager": false,
-              "recruting_lead": "Gaven Belson",
-              "direct_recruiting_manager": true,
-              "date_applied": "Mon Jul 17 2017",
-              "date_phone_interview": "Mon Jul 17 2017",
-              "date_submit": "Mon Jul 17 2017",
-              "date_interview": "Wed Jul 19 2017",
-              "date_followup": "Wed Jul 19 2017",
-              "comments": "Why is Gaven Belson working here",
-              "student_id": 3
-            },
-
-            {
-                  "company": "Black-Briar Meadery",
-                  "job_link": "Black-Briar.com",
-                  "industry": "selling mead fool",
-                  "location": "Riften",
-                  "job_title": "Bartender",
-                  "hiring_manager": "Ungrien",
-                  "direct_contact_manager": false,
-                  "recruting_lead": "Maven Black-Briar",
-                  "direct_recruiting_manager": true,
-                  "date_applied": "Mon Jul 17 2017",
-                  "date_phone_interview": "Mon Jul 17 2017",
-                  "date_submit": "Mon Jul 17 2017",
-                  "date_interview": "Wed Jul 19 2017",
-                  "date_followup": "Wed Jul 19 2017",
-                  "comments": "you can buy a bottle for 10 gold from Romlyn, and sell it else where for 25 gold",
-                  "student_id": 4
-                }
-
-
-
-
-
-
+          "company": "Pokemon",
+          "job_link": "pokemon.com",
+          "industry": "Training pokemon",
+          "location": "W. Pokemon lane 5555",
+          "job_title": "coder for the trainers",
+          "hiring_manager": "Umpa Loompa",
+          "direct_contact_manager": false,
+          "recruting_lead": "Ash",
+          "direct_recruiting_manager": false,
+          "date_applied": "Mon Jul 17 2017",
+          "date_phone_interview": "Mon Jul 17 2017",
+          "date_submit": "Mon Jul 17 2017",
+          "date_interview": "Wed Jul 19 2017",
+          "date_followup": "Wed Jul 19 2017",
+          "comments": "gotta catch em all",
+          "student_id": 1
+        }
+        {
+          "company": "Duder Mifflin Paper Company",
+          "job_link": "Paper.com",
+          "industry": "Making paper",
+          "location": "W. OFFCIE SPACE Rd. 999",
+          "job_title": "Printing paper",
+          "hiring_manager": "Milton",
+          "direct_contact_manager": false,
+          "recruting_lead": "Lunberg",
+          "direct_recruiting_manager": true,
+          "date_applied": "Mon Jul 17 2017",
+          "date_phone_interview": "Mon Jul 17 2017",
+          "date_submit": "Mon Jul 17 2017",
+          "date_interview": "Wed Jul 19 2017",
+          "date_followup": "Wed Jul 19 2017",
+          "comments": "if you could stay late thatd be great",
+          "student_id": 2
+        },
+        {
+          "company": "Pied piper",
+          "job_link": "piedpiper.com",
+          "industry": "Data compression",
+          "location": "Silicon Valley",
+          "job_title": "CTO",
+          "hiring_manager": "Erlech Bachman",
+          "direct_contact_manager": false,
+          "recruting_lead": "Gaven Belson",
+          "direct_recruiting_manager": true,
+          "date_applied": "Mon Jul 17 2017",
+          "date_phone_interview": "Mon Jul 17 2017",
+          "date_submit": "Mon Jul 17 2017",
+          "date_interview": "Wed Jul 19 2017",
+          "date_followup": "Wed Jul 19 2017",
+          "comments": "Why is Gaven Belson working here",
+          "student_id": 3
+        },
+        {
+          "company": "Black-Briar Meadery",
+          "job_link": "Black-Briar.com",
+          "industry": "selling mead fool",
+          "location": "Riften",
+          "job_title": "Bartender",
+          "hiring_manager": "Ungrien",
+          "direct_contact_manager": false,
+          "recruting_lead": "Maven Black-Briar",
+          "direct_recruiting_manager": true,
+          "date_applied": "Mon Jul 17 2017",
+          "date_phone_interview": "Mon Jul 17 2017",
+          "date_submit": "Mon Jul 17 2017",
+          "date_interview": "Wed Jul 19 2017",
+          "date_followup": "Wed Jul 19 2017",
+          "comments": "you can buy a bottle for 10 gold from Romlyn, and sell it else where for 25 gold",
+          "student_id": 4
+        }
       ]);
     });
 };
--- orion/routes/jobsRoutes.js
+++ orion/routes/jobsRoutes.js
@@ -11,4 +11,49 @@ router.get('/', (req, res, next) => {
   });
 });
 
+/*
+  ADD a jobs
+*/
+router.post('/', (req, res, next) => {
+  knex('jobs').insert(req.body).then(() => {
+    res.send('successfully inserted new row in jobs');
+  });
+});
+
+/*
+  GET all jobs for a student
+*/
+router.get('/student/:id', (req, res, next) => {
+  knex('jobs').where('student_id', req.params.id).then((jobs) => {
+    res.send(jobs);
+  });
+});
+
+/*
+  GET one job
+*/
+router.get('/:id', (req, res, next) => {
+  knex('jobs').where('id', req.params.id).then((jobs) => {
+    res.send(jobs);
+  });
+});
+
+/*
+  DELETE one job
+*/
+router.get('/delete/:id', (req, res, next) => {
+  knex('jobs').where('id', req.params.id).del().then(() => {
+    res.send('successfully deleted a job');
+  });
+});
+
+/*
+  EDIT one job
+*/
+router.post('/:id', (req, res, next) => {
+  knex('jobs').where('id', req.params.id).update(req.body).then(() => {
+    res.send('successfully updated a job');
+  });
+});
+
 module.exports = router;
