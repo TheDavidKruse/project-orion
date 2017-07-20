9236b1d8cae203e25d600a3b8f4b8ff94feb1799
theDavidKruse <thedavidkruse@gmail.com>
2017-07-20 10:07:32 -0700

    Fixing profile pages

--- orion/public/nav-hide.js
+++ orion/public/nav-hide.js
@@ -18,5 +18,9 @@ tablinks[i].className = tablinks[i].className.replace(" active", "");
 // Show the current tab, and add an "active" class to the button that opened the tab
 document.getElementById(name).style.display = "block";
 evt.currentTarget.className += " active";
+
+
 }
-document.getElementById("home").click();
+var showDiv = () => {
+  document.getElementById("home").style.display="";
+}
