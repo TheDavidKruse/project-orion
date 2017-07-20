38618f6b67362ee98917fb628955f8fd7b13202a
Drienn <Emitdutcher@gmail.com>
2017-07-20 12:49:46 -0700

    edits to staff/student routes

--- orion/routes/studentRoutes.js
+++ orion/routes/studentRoutes.js
@@ -23,17 +23,27 @@ router.get('/', function(req, res) {
   });
 });
 
-router.get('/:id', function(req, res){
-  knex('student').where('id', req.params.id).then(function(student) {
-    res.render('student-layout', {
-      student
-    });
-  });
-});
+router.get('/:id', function(req, res, next) {
+  let sqlArr = [knex('staff').select().where('id', req.params.id),
+    knex.raw(`select student_todo.*, student.first_name as student_first, student.last_name as student_last, student.staff_id, staff.first_name as staff_first, staff.last_name as staff_last, todo.id, todo.name as assignment_name, todo.description, todo.status from student_todo join student on student.id = student_todo.student_id join staff on staff.id = student.staff_id join todo on todo.id = student_todo.todo_id where staff.id = ${req.params.id}`),
+    knex('student').select(),
+    knex('contacts').select(),
+    knex('jobs').select(),
+  ];
+  Promise.all(sqlArr).then(result => res.render('student-layout', {
+    staff: result[0],
+    staffHome: result[1].rows,
+    students: result[2],
+    contacts: result[3],
+    jobs: result[4],
+    selectedContact: false,
+    selectedJob: false
+  }))
+})
 
 router.get('/:id/edit', function(req, res, next) {
   knex('student').select().where('id', req.params.id).then(function(student) {
-      console.log(student);
+    console.log(student);
     res.render('student-layout', {
       student
     });
@@ -41,14 +51,14 @@ router.get('/:id/edit', function(req, res, next) {
 });
 
 
-router.post('/:id', function(req, res, next){
+router.post('/:id', function(req, res, next) {
   knex('student').update(req.body).where('id', req.params.id)
-  .then(function(){
-    knex('student').then(function(student){
+    .then(function() {
+      knex('student').then(function(student) {
         res.render('student-layout', {
           student
         });
       });
-});
+    });
 });
 module.exports = router;
